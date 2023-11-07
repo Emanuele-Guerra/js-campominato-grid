21f683ba20d84efb1cff0d9d1870e2fa5204463a
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-07 16:18:41 +0100

    push2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+
+
+
 let numCella = 100
 console.log("daje")
 
 let grid = document.getElementById("grid")
 let cella = "";
 
-for (let i = 1; i <= cella; i++) {
-    cella.document.createElement("div")
-    cella.classList.add("easy-cell")
-    console.log(i)
+
+for (let i = 1; i <= numCella; i++) {
+    const cella =  document.createElement("div")
+     cella.classList.add("easy-cell")
+     grid.appendChild(cella)
     
 }
