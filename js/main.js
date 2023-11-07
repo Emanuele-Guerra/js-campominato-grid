3faef81fcf8999ead8daf860c86f6846cac93131
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-07 17:34:51 +0100

    pushh

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 let mode = document.getElementById("label");
 let start = document.getElementById("start")
 
-let modevalore = mode.value;
+
 
 
 let grid = document.getElementById("grid")
 let cella = "";
-let numCella = "";
-start.addEventListener("click", function(){
+let numCella = "10";
 
+start.addEventListener("click", function(){
+    let modevalore = mode.value;
+    console.log(modevalore)
     if (modevalore == "easy"){
         numCella = 100
     }   else if (modevalore == "medium"){
@@ -18,11 +20,23 @@ start.addEventListener("click", function(){
         }
                
         for (let i = 1; i <= numCella; i++) {
-    
-            const cella =  document.createElement("div")
-            cella.classList.add("easy-cell")
-            grid.appendChild(cella)
-            cella.innerHTML = ([i])        
+            if (modevalore = "easy"){
+                let cella =  document.createElement("div")
+                cella.classList.add("easy-cell")
+                grid.appendChild(cella)
+                cella.innerHTML = ([i])   
+            } else if (modevalore = "medium"){
+                let cella =  document.createElement("div")
+                cella.classList.add("mid-cell")
+                grid.appendChild(cella)
+                cella.innerHTML = ([i])
+            } else if (modevalore == "hard"){
+                let cella =  document.createElement("div")
+                cella.classList.add("hard-cell")
+                grid.appendChild(cella)
+                cella.innerHTML = ([i])
+            }
+                 
         
             cella.addEventListener("click", function(){
                 this.classList.toggle("clicked")
@@ -31,11 +45,11 @@ start.addEventListener("click", function(){
     
     
             })
-        }
+       
         
-
+        }
 })
 
-    
+ 
  
         
