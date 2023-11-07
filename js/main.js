fa5f894c102c88373f1f1af36cc3288742bfd465
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-07 17:10:00 +0100

    push3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
+let mode = document.getElementById("label");
+let start = document.getElementById("start")
 
+let modevalore = mode.value;
 
-
-let numCella = 100
-console.log("daje")
 
 let grid = document.getElementById("grid")
 let cella = "";
+let numCella = "";
+start.addEventListener("click", function(){
 
-
-for (let i = 1; i <= numCella; i++) {
-    const cella =  document.createElement("div")
-     cella.classList.add("easy-cell")
-     grid.appendChild(cella)
+    if (modevalore == "easy"){
+        numCella = 100
+    }   else if (modevalore == "medium"){
+                    numCella = 81
+        }   else if (modevalore == "hard"){
+            numCella = 49
+        }
+               
+        for (let i = 1; i <= numCella; i++) {
     
-}
+            const cella =  document.createElement("div")
+            cella.classList.add("easy-cell")
+            grid.appendChild(cella)
+            cella.innerHTML = ([i])        
+        
+            cella.addEventListener("click", function(){
+                this.classList.toggle("clicked")
+                
+                
+    
+    
+            })
+        }
+        
+
+})
+
+    
+ 
+        
